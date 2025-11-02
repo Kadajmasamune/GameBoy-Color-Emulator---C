eba177fd02ea9f14105bda9144b39ce7ed3c6cd9
Rayan Ul Haq (12th Remnant) <Rayanhaq1234@gmail.com>
2025-11-02 21:31:32 +0500

    Init

---
+++ GameBoyColor/src/include/Common.h
@@ -0,0 +1,14 @@
+//
+// Created by Rayan on 11/1/2025.
+//
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <string.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif //COMMON_H
